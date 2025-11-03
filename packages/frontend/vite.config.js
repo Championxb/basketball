1355dbd774b77273469b6fb0548cdc3516b911c9
chengxianbin <1066101069@qq.com>
2025-11-03 21:35:18 +0800

    ⚙️config: 局域网配置

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true, // 0.0.0.0, 允许局域网访问
+    port: 5173,
+    strictPort: false,
+    cors: true,
+  },
 })
+ 
