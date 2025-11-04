9e6df7e2aaff2bf6c8bd8effc9f7e38829d96def
程先斌 <1066101069@qq.com>
2025-11-05 01:01:26 +0800

    🎨update: home组件修改

--- packages/frontend/src/views/Home.vue
+++ packages/frontend/src/views/Home.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="container">
-    <h1>欢迎来到首页</h1>
+    <!-- <img src="@assets/header.svg" alt="" /> -->
+    <Header />
+    <div class="wrapper">
+      <h1>欢迎来到首页</h1>
+      <div class="box"></div>
+    </div>
   </div>
 </template>
+<script setup>
+import Header from "@/components/header.vue";
+</script>
 <style lang="scss" scoped>
 @keyframes gradientFlow {
   0% {
@@ -16,15 +24,28 @@
   }
 }
 .container {
+  width: 100vw;
   height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   font-family: "Orbitron", sans-serif;
-  text-shadow: 0 0 10px rgba(255, 255, 255, 0.5);
+  font-size: font(32px);
+  text-shadow: 0 0 10px rgba(230, 237, 207, 0.5);
 
   background: linear-gradient(-45deg, #3a1c71, #d76d77, #2ebf91, #00c9ff);
+  background: rgb(173, 172, 211);
   background-size: 400% 400%;
   animation: gradientFlow 10s ease infinite;
+
+  .wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    .box {
+      width: vw(200px);
+      height: vh(200px);
+      background-color: rgba(255, 255, 255, 0.2);
+    }
+  }
 }
 </style>
